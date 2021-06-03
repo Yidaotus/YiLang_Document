6b3678f3f3e4f0826960d51d063fd40acb4ed82a
Daniel Voigt <yidaotus@protonmail.com>
2021-06-03 17:35:15 +0200

    Factor out Dictionary types

---
+++ Dictionary.ts
@@ -0,0 +1,57 @@
+import { IDocument } from './Document';
+import { UUID } from './UUID';
+
+export interface ITextPosition {
+	start: number;
+	end: number;
+}
+
+export interface ISource<T> {
+	pos: ITextPosition;
+	source: T;
+}
+
+export interface ISentence {
+	content: string;
+	translation: string;
+}
+
+export interface IDictionaryEntry {
+	id: UUID;
+	key: string;
+	lang: string;
+	translations: string[];
+	sourceDocument?: ISource<IDocument>;
+	firstSeen?: ISource<ISentence>;
+	tags: UUID[];
+	// type: IEntryType;
+	comment?: string;
+	spelling?: string;
+	variations: IDictionaryVariant[];
+}
+
+export interface IDictionaryVariant {
+	key: string;
+	tags?: UUID[];
+	comment: string;
+	spelling?: string;
+}
+
+export interface IDictionaryLookupSource {
+	lang: string;
+	name: string;
+	source: string;
+}
+
+export interface IGrammarPoint {
+	name: string;
+	description: string;
+	construction: string[];
+}
+
+export interface IDictionaryTag {
+	id: UUID;
+	name: string;
+	color?: string;
+	grammarPoint?: IGrammarPoint; // Te versions are used for conjugation ect..
+}
--- Document.ts
+++ Document.ts
@@ -29,61 +29,6 @@ export interface IEntrySubType {
 	comment?: string; // Te versions are used for conjugation ect..
 }
 
-export interface IGrammarPoint {
-	name: string;
-	description: string;
-	construction: string[];
-}
-
-export interface IEntryTag {
-	id: UUID;
-	name: string;
-	color?: string;
-	grammarPoint?: IGrammarPoint; // Te versions are used for conjugation ect..
-}
-
-export interface ISentence {
-	content: string;
-	translation: string;
-}
-
-export interface ITextPosition {
-	start: number;
-	end: number;
-}
-
-export interface ISource<T> {
-	pos: ITextPosition;
-	source: T;
-}
-
-export interface IDictionaryEntry {
-	id: UUID;
-	key: string;
-	lang: string;
-	translations: string[];
-	sourceDocument?: ISource<IDocument>;
-	firstSeen?: ISource<ISentence>;
-	tags: UUID[];
-	// type: IEntryType;
-	comment?: string;
-	spelling?: string;
-	variations: IDictionaryVariant[];
-}
-
-export interface IDictionaryVariant {
-	key: string;
-	tags?: UUID[];
-	comment: string;
-	spelling?: string;
-}
-
-export interface IDictionaryLookupSource {
-	lang: string;
-	name: string;
-	source: string;
-}
-
 export interface ITextChunk {
 	text?: string;
 }
